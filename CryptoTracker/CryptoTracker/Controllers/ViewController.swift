78e4e99603e6ea4196663397cf5fa805814e8038
hanh1287 <nguyen.huy.ha@sun-asterisk.com>
2022-08-10 11:14:50 +0700

    [Task][52074] Apply MVC Architecture

---
+++ CryptoTracker/CryptoTracker/Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Huy Hà on 8/10/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
